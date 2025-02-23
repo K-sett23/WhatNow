19b1bfb86cf88288c377afd70f96016727146194
Saul Herrera <herrerasaul64@gmail.com>
2025-02-23 12:29:59 -0600

    Actualizacion de AppNavigator

--- navigation/AppNavigator.tsx
+++ navigation/AppNavigator.tsx
@@ -3,23 +3,19 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from '../screens/HomeScreen';
 import HistoryScreen from '../screens/HistoryScreen';
+import LoginScreen from '../screens/LoginScreen';
+import RegisterScreen from '../screens/RegisterScreen';
 
 const Stack = createStackNavigator();
 
 const AppNavigator: React.FC = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ title: 'WhatNow?' }}
-        />
-        <Stack.Screen
-          name="History"
-          component={HistoryScreen}
-          options={{ title: 'Historial' }}
-        />
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} options={{ title: 'Iniciar Sesión' }} />
+        <Stack.Screen name="Register" component={RegisterScreen} options={{ title: 'Registro' }} />
+        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'WhatNow?' }} />
+        <Stack.Screen name="History" component={HistoryScreen} options={{ title: 'Historial' }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
